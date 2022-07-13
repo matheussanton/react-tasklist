95a21a44ffa96ff67a8db5f1bcacbeefe3cd939d
Matheus Santon Galter <matheussantong@gmail.com>
2022-07-12 22:04:01 -0300

    added completed management and exclusion function

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import {v4 as uuidv4} from 'uuid';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import Tasks from "./components/Tasks";
+import AddTask from './components/AddTask';
+import Header from './components/Header';
+
+import './App.css';
+
+const App = () => {
+
+  const [tasks, setTasks] = useState([
+    { 
+      id: '1',
+      title: 'Estudar programação',
+      completed: false
+    },
+    { 
+      id: '2',
+      title: 'Ler livros',
+      completed: true
+    }
+  ]);
+  
+  const handleTaskClick = (taskId) => {
+
+    const newTasks = tasks.map(task => {
+      if(task.id == taskId) return {
+        ... task, completed: !task.completed
+      }
+      
+      return task;
+    });
+
+    setTasks(newTasks);
+  };
+  
+  const handleTaskDeletion = (taskId) => {
+      const newTasks = tasks.filter(task => task.id != taskId)
+  
+      setTasks(newTasks);
+  }
+  
+  const handleTaskAddition = (taskTitle) => {
+    const newTask = [
+      ... tasks, 
+      { 
+        title: taskTitle,
+        id: uuidv4(),
+        completed: false,
+    },
+  ];
+
+  setTasks(newTask);
+};
+
+
+  return( 
+    <Router>
+      <div className='container'>
+        <Header/>
+        <Route 
+        path="/"
+        //exact 
+          render={() => (
+            <>
+              <AddTask handleTaskAddition={handleTaskAddition}/>
+              <Tasks tasks={tasks}
+                handleTaskClick={handleTaskClick}
+                handleTaskDeletion={handleTaskDeletion}/>
+            </>
+          )}
+        />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
